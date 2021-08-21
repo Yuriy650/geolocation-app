8ac9b47612c637d51bf038200a2e4260d4d2a5c8
Yuriy Liubinskyi <leorentex@gmail.com>
2021-08-22 00:04:07 +0300

    deploy geolocation-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+import {IpGeolocationData} from "./interfaces";
+import Content from "./Content/Content";
+import '@fontsource/roboto';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState<IpGeolocationData[]>([]);
+    const [tempIp, setTempIp] = useState<string>('');
+    const [ip, setIp] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    useEffect(() => {
+        const arr: IpGeolocationData[] = [];
+        //eslint-disable-next-line
+        fetch(`https://ipapi.co/` + `${ip}` + `/json/`)
+            .then(response => response.json())
+            .then(json => {
+                    setIsLoaded(true)
+                    arr.push(json);
+                    setData(arr)
+                },
+                (error) => {
+                    setIsLoaded(true)
+                    console.log(error.message)
+                    setError(error.message);
+                })
+    }, [ip])
+    return (
+        <div className="app-container">
+            <div className="app-container__body">
+                <div className="app-container__form">
+                    <div className="app-container__form-title">
+                        <div className="app-container__image"> </div>
+                        <p className="app-container__title">Find My IP</p>
+                    </div>
+                    <form>
+                        <input
+                            type="text"
+                            className="app-container__form-input"
+                            placeholder="IP"
+                            name="comment"
+                            value={tempIp}
+                            onChange={(e) => setTempIp(e.currentTarget.value)}/>
+                        <button type="button" className="app-container__form-btn"
+                                onClick={() => setIp(tempIp)}>Search
+                        </button>
+                    </form>
+                </div>
+                <Content
+                    data={data}
+                    error={error}
+                    isLoaded={isLoaded}
+                    ip={ip}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Content/Content.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import './content.scss';
+import {IpGeolocationData} from "../interfaces";
+import "@fontsource/roboto-mono";
+
+const Content = (props: any) => {
+    if (props.error) {
+        return  (
+            <div className="error">Something went wrong 😞. Please try again. Error: {props.error}</div>
+        )
+    } else if (!props.isLoaded) {
+        return <div className="loader">Loading...</div>;
+    } else if (!props.ip) {
+        return <div className="message">Enter IP and press “Search” to get geolocation data</div>
+    }
+
+    return (
+        <div className="content-container">
+            {props.data.map((item:IpGeolocationData)=>(
+                <ul key={item.ip} className="content-container__list">
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>ip: <span className="content-container__string">{item.ip}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>version:<span className="content-container__string">{item.version}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>city: <span className="content-container__string">{item.city}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>region: <span className="content-container__string">{item.region}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>region_code: <span className="content-container__string">{item.region_code}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>country_code: <span className="content-container__string">{item.country_code}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>country_name: <span className="content-container__string">{item.country_name}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>country_capital: <span className="content-container__string">{item.country_capital}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>country_tld: <span className="content-container__string">{item.country_tld}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>continent_code: <span className="content-container__string">{item.continent_code}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-boolean">tf</button>
+                        <p>in_eu: <span className="content-container__boolean">{String(item.in_eu)}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>postal: <span className="content-container__string">{item.postal}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-number">#</button>
+                        <p>latitude: <span className="content-container__number">{item.latitude}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-number">#</button>
+                        <p>longitude: <span className="content-container__number">{item.longitude}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>timezone: <span className="content-container__string">{item.timezone}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>utc_offset: <span className="content-container__string">{item.utc_offset}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>country_calling_code: <span className="content-container__string">{item.country_calling_code}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>currency: <span className="content-container__string">{item.currency}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>currency_name: <span className="content-container__string">{item.currency_name}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>languages: <span className="content-container__string">{item.languages}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-number">#</button>
+                        <p>country_area: <span className="content-container__number">{item.country_area}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-number">#</button>
+                        <p>country_population: <span className="content-container__number">{item.country_population}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>asn: <span className="content-container__string">{item.asn}</span></p>
+                    </li>
+                    <li>
+                        <button className="content-container__btn-string">“</button>
+                        <p>org: <span className="content-container__string">{item.org}</span></p>
+                    </li>
+                </ul>
+            ))}
+        </div>
+    )
+}
+
+export default Content;
